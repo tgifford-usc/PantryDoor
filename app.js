a4ca06b80efb129602e1735ff19f49e9ab63e8c0
Dr Offig <dr.offig@offig.com>
2023-11-02 09:10:02 +1000

    First Working Version

---
+++ app.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import fetch from 'node-fetch';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+// Ukraine Alerts API
+const apiBase = "https://getpantry.cloud/apiv1/pantry/";
+const locationsEndpoint = "/basket/locations";
+
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+});
+
+
+app.get("/status", (request, response) => {
+    const status = {
+       "Status": "Running"
+    };
+    response.send(status);
+});
+
+
+app.put("/", async (request, response) => {
+    console.log("Received locations request");
+    try {
+        const url = `${apiBase}${request.query["token"]}${locationsEndpoint}`;
+        const apiResponse = await fetch(url, {
+          method: 'PUT',
+          headers: {
+              Accept: "application/json",
+          },
+          body: request.body
+        });
+        const locations = await apiResponse.json();
+        response.send(locations);
+    } catch(err) {
+      console.log("Error processing locations request: ", err);
+    }
+});
