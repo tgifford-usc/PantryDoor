4a160ae2be7603fc55cf4001d60ec17479c57387
tgifford-usc <140787784+tgifford-usc@users.noreply.github.com>
2023-11-02 14:49:08 +1000

    Updated description of API

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
-// Ukraine Alerts API
+// Pantry API
 const apiBase = "https://getpantry.cloud/apiv1/pantry/";
 const locationsEndpoint = "/basket/locations";
 
